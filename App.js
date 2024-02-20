27a875761cae432e154c8c2a37f3ffbdcc6acafc
Minkyu0424 <yng1404@gmail.com>
2024-02-20 16:48:33 +0900

    :sparkels:[feat] : 지리정보 가져오기

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from "expo-status-bar";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
 import { StyleSheet, Dimensions, Text, View, ScrollView } from "react-native";
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
+
 export default function App() {
+  const [city, setCity] = useState('...Loading')
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    
+    if (!granted) {
+      setOk(false);
+    }
+
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync();
+    
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+
+    setCity(location[0].city);
+  };
+
+  useEffect(() => {
+    ask();
+  });
+  
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Yatap</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         horizontal
