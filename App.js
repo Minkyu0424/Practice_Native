0eaa8d8f5186e553f7e71c48ad1eb75fe4ad319d
Minkyu0424 <yng1404@gmail.com>
2024-02-20 18:37:32 +0900

    :sparkels:[feat] : 날씨 정보 가져오기

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
 import * as Location from "expo-location";
 import { useEffect, useState } from "react";
-import { StyleSheet, Dimensions, Text, View, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Dimensions,
+  Text,
+  View,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
+// const API_KEY = process.env.REACT_APP_WEATHER_KEY;
+
+const API_KEY = "9f3f1976fc721cbf0acf60f5608efe89";
 
 export default function App() {
-  const [city, setCity] = useState('...Loading')
-  const [location, setLocation] = useState();
+  const [city, setCity] = useState("...Loading");
+  const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
 
-  const ask = async () => {
+  const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
-    
+
     if (!granted) {
       setOk(false);
     }
@@ -18,19 +28,32 @@ export default function App() {
     const {
       coords: { latitude, longitude },
     } = await Location.getCurrentPositionAsync();
-    
+
     const location = await Location.reverseGeocodeAsync(
       { latitude, longitude },
       { useGoogleMaps: false }
     );
 
     setCity(location[0].city);
+
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+
+    const json = await res.json();
+    setDays(
+      json.list.filter((weather) => {
+        if (weather.dt_txt.includes("00:00:00")) {
+          return weather;
+        }
+      })
+    );
   };
 
   useEffect(() => {
-    ask();
-  });
-  
+    getWeather();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
@@ -39,25 +62,25 @@ export default function App() {
       <ScrollView
         horizontal
         pagingEnabled
-        showsHorizontalScrollIndicator={false}
-        style={styles.weather}
+        contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>13</Text>
-          <Text style={styles.des}>Cloudy</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>13</Text>
-          <Text style={styles.des}>Cloudy</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>13</Text>
-          <Text style={styles.des}>Cloudy</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>13</Text>
-          <Text style={styles.des}>Cloudy</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator
+              color="white"
+              size="large"
+              style={{ marginTop: 10 }}
+            />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <Text style={styles.temp}>{parseFloat(day.main.temp).toFixed(1)}</Text>
+              <Text style={styles.des}>{day.weather[0].main}</Text>
+              <Text style={styles.t}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
@@ -85,10 +108,10 @@ const styles = StyleSheet.create({
   },
   temp: {
     marginTop: 50,
-    fontSize: 178,
+    fontSize: 158,
   },
   des: {
     marginTop: -30,
-    fontSize: 52,
+    fontSize: 72,
   },
 });
