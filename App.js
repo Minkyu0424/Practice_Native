4e24c8e1d42dce738d5863c90c80e39f8c310bf3
Minkyu0424 <yng1404@gmail.com>
2024-02-20 16:01:39 +0900

    :lipstick:[design] UI공부 및 적용

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Dimensions, Text, View, ScrollView } from "react-native";
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Yatap</Text>
+      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        style={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>13</Text>
+          <Text style={styles.des}>Cloudy</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>13</Text>
+          <Text style={styles.des}>Cloudy</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>13</Text>
+          <Text style={styles.des}>Cloudy</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>13</Text>
+          <Text style={styles.des}>Cloudy</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: { flex: 1, backgroundColor: "#c08552" },
+  city: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#dab49d",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 58,
+    fontWeight: "bold",
+  },
+  weather: {
+    backgroundColor: "#c08552",
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  des: {
+    marginTop: -30,
+    fontSize: 52,
   },
 });
